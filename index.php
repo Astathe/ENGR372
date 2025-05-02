e1427314d88905422704d949ada05fec18191a25
Kerem <43040049+Astathe@users.noreply.github.com>
2025-05-02 14:54:37 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+require_once 'config/database.php';
+
+// Get all tournaments
+$stmt = $pdo->query("SELECT * FROM tournaments");
+$tournaments = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,13 +20,10 @@
     <div class="wallpaper" style="background-image: url('images/image1.jpg');"></div>
     <nav class="top-bar">
         <div class="logo">
-            <a href="#">My Website</a>
+            <a href="/index.php">My Website</a>
         </div>
         <ul class="nav-links">
-            <li><a href="/index.html" class="active">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Contact</a></li>
+            <li><a href="/index.php" class="active">Home</a></li>
         </ul>
         <div class="mobile-menu">
             <span></span>
@@ -28,42 +33,14 @@
     </nav>
     <main>
         <div class="item-grid">
-            <a href="#" class="item-box">
+            <?php foreach ($tournaments as $tournament): ?>
+            <a href="tournament.php?id=<?php echo $tournament['tournament_id']; ?>" class="item-box">
                 <div class="item-image">
-                    <img src="images/item1.png" alt="Item 1">
+                    <img src="<?php echo htmlspecialchars($tournament['tournament_logo']); ?>" alt="<?php echo htmlspecialchars($tournament['tournament_name']); ?>">
                 </div>
-                <h3 class="item-title">Ziraat Türkiye Kupası</h3>
+                <h3 class="item-title"><?php echo htmlspecialchars($tournament['tournament_name']); ?></h3>
             </a>
-            <a href="./tournament.html" class="item-box">
-                <div class="item-image">
-                    <img src="images/item2.jpg" alt="Item 2">
-                </div>
-                <h3 class="item-title">PGL Major Copenhagen 2024</h3>
-            </a>
-            <a href="#" class="item-box">
-                <div class="item-image">
-                    <img src="images/item3.jpg" alt="Item 3">
-                </div>
-                <h3 class="item-title">Item 3</h3>
-            </a>
-            <a href="#" class="item-box">
-                <div class="item-image">
-                    <img src="images/item4.jpg" alt="Item 4">
-                </div>
-                <h3 class="item-title">Item 4</h3>
-            </a>
-            <a href="#" class="item-box">
-                <div class="item-image">
-                    <img src="images/item5.jpg" alt="Item 5">
-                </div>
-                <h3 class="item-title">Item 5</h3>
-            </a>
-            <a href="#" class="item-box">
-                <div class="item-image">
-                    <img src="images/item6.jpg" alt="Item 6">
-                </div>
-                <h3 class="item-title">Item 6</h3>
-            </a>
+            <?php endforeach; ?>
         </div>
     </main>
     <footer class="footer">
@@ -73,9 +50,6 @@
             </div>
             <ul class="nav-links">
                 <li><a href="#" class="active">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Contact</a></li>
             </ul>
         </div>
         <div class="footer-bottom">
